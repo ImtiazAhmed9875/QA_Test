6868261a89c987ad785b92327487419304912195
imtiazahmed9875 <imtiazahmed9875@gmail.com>
2025-04-18 13:04:37 +0600

    Push All code

--- src/main/java/org/example/Page/Registration.java
+++ src/main/java/org/example/Page/Registration.java
@@ -13,7 +13,7 @@ public Registration(WebDriver driver) {
     driver.get("https://labsqajobs.qaharbor.com/candidate-registration/");
 }
 
-
+    By Signup = By.xpath("//*[@id=\"jet-theme-core-header\"]/div/div/section[1]/div[2]/div[2]/div/div[3]/div/nav/div[2]/div/div/a/span");
     By UserName = By.xpath("//input[@name=\"username\"]");
     By Email = By.xpath("//input[@name=\"email\"]");
     By Password = By.xpath("//input[@name=\"password\"]");
@@ -21,12 +21,15 @@ public Registration(WebDriver driver) {
     By Reg = By.xpath("//button[@class=\"jet-form-builder__action-button jet-form-builder__submit submit-type-reload\"]");
 
     public void reg(String User,String email,String pass,String cpass){
-
+        driver.findElement(Signup).click();
+        Select select = new Select(driver.findElement(Signup));
+        select.selectByVisibleText("Candidate");
         driver.findElement(UserName).sendKeys(User);
         driver.findElement(Email).sendKeys(email);
         driver.findElement(Password).sendKeys(pass);
         driver.findElement(ConPasswprd).sendKeys(cpass);
         driver.findElement(Reg).click();
+
 }
 
 }
